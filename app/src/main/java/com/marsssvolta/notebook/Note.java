799cc5b01fdfd2bdcd9b1f9ac219324b83d902e0
Marsssvolta <marsssvolta@gmail.com>
2018-11-29 13:41:53 +0400

    Добавлеине адаптера и entity

---
+++ app/src/main/java/com/marsssvolta/notebook/Note.java
@@ -0,0 +1,24 @@
+package com.marsssvolta.notebook;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "note")
+    private String mNote;
+
+    public Note(@NonNull String note) {
+        this.mNote = note;
+    }
+
+    @NonNull
+    public String getNote() {
+        return this.mNote;
+    }
+}
