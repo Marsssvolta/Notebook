d265fdb889db8613362db12dd9eca3cc27946adf
Marsssvolta <marsssvolta@gmail.com>
2018-12-29 10:28:25 +0400

    Очистка database

--- app/src/main/java/com/marsssvolta/notebook/Model/NoteRoomDatabase.java
+++ app/src/main/java/com/marsssvolta/notebook/Model/NoteRoomDatabase.java
@@ -1,12 +1,9 @@
 package com.marsssvolta.notebook.Model;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteRoomDatabase extends RoomDatabase {
@@ -22,34 +19,10 @@ public abstract class NoteRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             NoteRoomDatabase.class, "note_database")
                             .fallbackToDestructiveMigration()
-                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
-    }
-
-    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
-
-        @Override
-        public void onOpen(@NonNull SupportSQLiteDatabase db) {
-            super.onOpen(db);
-            new PopulateDbAsync(INSTANCE).execute();
-        }
-    };
-
-    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
-
-        private final NoteDao mDao;
-
-        PopulateDbAsync(NoteRoomDatabase db) {
-            mDao = db.noteDao();
-        }
-
-        @Override
-        protected Void doInBackground(final Void... params) {
-            return null;
-        }
     }
 }
